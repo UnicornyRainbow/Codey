9b086bb25bcba5b7f53b24e0857dae35c64b055d
blackmoonboy <blackforlive@protonmail.com>
2022-02-11 21:02:41 +0100

    Added installscript and .desktop file

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir ~/sourcefiles
+mkdir ~/sourcefiles/unicorn_codey
+cp codey.py ~/sourcefiles
+cp codey.config ~/sourcefiles
+cp unicorn_codey.desktop ~/.local/share/applications
+cp unicorn_codey.png ~/.local/share/icons
